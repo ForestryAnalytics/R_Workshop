fd0ac76c8483c69bf858dc16accb6fa7b1d13ba1
DragonflyStats <kobriendublin@gmail.com>
2018-10-01 22:52:23 +0100

    Add files via upload

---
+++ 00packages.R
@@ -0,0 +1,17 @@
+# Run All The Following Code
+# Select "Ireland" as mirror
+
+install.packages("sp")
+install.packages("rgdal")
+install.packages("rgeos")
+install.packages("spdep")
+install.packages("ggmap")
+
+######################################
+
+library(sp)       # vector data
+library(raster)   # raster data
+library(rgdal)    # input/output, projections
+library(rgeos)    # geometry ops
+library(spdep)    # spatial dependence
+library(ggmap)    # ggmap package
